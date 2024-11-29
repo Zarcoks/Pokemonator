6c346291f992db8e77705202b8a989ab7acfbce8
tractobn57 <justinegierlo57@gmail.com>
2024-11-29 15:23:22 -0500

    info des pokemon quand tu passes la souris

--- src/components/LivePokemons.vue
+++ src/components/LivePokemons.vue
@@ -25,14 +25,32 @@ import type {Pokemon} from "@/librairies/api";
     <h2>Les pokemons auxquels je pense...</h2>
     <div>
         <article v-for="pok in trierGrised()" :class="{grised:(isGrised(pok))}">
+          <div id="base">
             <img :src="pok.image" alt="pik"/>
             <span>{{ pok.nom }}</span>
+          </div>
+          <div id="infos">
+            <span> numéro pokédex : {{ pok.pokedex }}</span>
+            <span> type : {{pok.type}}</span>
+            <span> phase chaine d'évolution : {{pok.nivEvolution}}</span>
+            <span> poids : {{pok.poids}}</span>
+            <span> taille : {{pok.taille}}</span>
+            <span> couleur : {{pok.couleur}}</span>
+            <span> bebe : {{pok.bebe}}</span>
+            <span> legendaire : {{pok.legendaire}}</span>
+            <span> mythique : {{pok.mythique}}</span>
+            <span> habitat : {{pok.habitat}}</span>
+            <span> forme : {{pok.forme}}</span>/
+            <span> évènement : {{pok.evenement}}</span>
+            <span> Objet pour évoluer : {{pok.objetEvoltution}}</span>
+          </div>
         </article>
     </div>
   </div>
 </template>
 
 <style scoped>
+
     #pokemons {
       display: flex;
       flex-direction: column;
@@ -44,10 +62,11 @@ import type {Pokemon} from "@/librairies/api";
         flex-direction: column;
         overflow: scroll;
         width: 300px;
-        height: 200px;
+        height: 300px;
         background-color: lightgray;
         border-radius: 20px;
         border: solid 1px;
+
     }
 
     .aligneGif {
@@ -60,19 +79,48 @@ import type {Pokemon} from "@/librairies/api";
         margin: 5px;
     }
 
-    article {
+    article > #base {
         display: flex;
         align-items: center;
+
         background-color: rgb(230, 230, 230);
         border-bottom: solid 1px;
+        overflow-y : hidden;
+
     }
+
+
+    #infos {
+      display: none;
+    }
+
+    #infos > span {
+      display: block;
+      align-items: center;
+
+    }
+
+    article:hover {
+      background-color: rgb(252, 252, 252);
+      transition: all 200ms ease-out;
+
+    }
+
+    article:hover > #base{
+      border-bottom: none;
+    }
+
+    article:hover > #infos {
+      display: block;
+      opacity: 1;
+      padding: 10px;
+      border-bottom: solid 1px;
+    }
+
 
     .grised {
         background-color: rgb(105, 105, 105);
     }
 
-    article:hover {
-        background-color: rgb(252, 252, 252);
-        transition: all 200ms ease-out;
-    }
+
 </style>
