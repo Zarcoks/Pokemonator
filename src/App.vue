7f3208ad6e7bd1dcc8bdf2fb4d6dac6d1de26f13
Victor <victorjost34@gmail.com>
2024-12-02 15:10:17 -0500

    Page de start

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,22 @@
     import { getNextQuestion, updateData } from './librairies/poserquestion';
     import Guesser from './components/Guesser.vue';
     
+    let imgProfesseur = ref(new URL('./assets/chen-akinator.png', import.meta.url).href)
+
     let workingOnPokemons = ref([...pokemons]) // La liste de tous les pokemons à modifier
     let impossiblePokemons = ref(new Array<Pokemon>()) // La liste des pokemons éliminés par les questions
     let question = ref(getNextQuestion(workingOnPokemons.value)); // La question loadé dynamiquement
 
     // Variables d'état, pour déterminer quel composant est à afficher (une seule à la fois)
+    let isNotStarted = ref(true)
     let isGuessing = ref(false)
-    let isAsking = ref(true)
+    let isAsking = ref(false)
     let isLoosing = ref(false)
+
+    function start() {
+        allFalse()
+        isAsking.value = true;
+    }
 
     function replay() {
         impossiblePokemons.value = new Array<Pokemon>()
@@ -24,6 +32,7 @@
     }
 
     function allFalse() {
+        isNotStarted.value = false
         isGuessing.value = false
         isAsking.value = false
         isLoosing.value = false
@@ -35,6 +44,10 @@
             allFalse()
             isGuessing.value = true            
         }
+        else if (workingOnPokemons.value.length === pokemons.length) {
+            allFalse()
+            isNotStarted.value = true
+        }
         else if (workingOnPokemons.value.length < 1){
             allFalse()
             isLoosing.value = true
@@ -42,14 +55,24 @@
         else if (workingOnPokemons.value.length > 1) {
             allFalse()
             isAsking.value = true;            
-        } 
+        }
     }
 </script>
 
 <template>
     <toolbar/>
     <main>
-        <div :class="{hidden: isGuessing}">
+        <div :class="{hidden: !isNotStarted}">
+            <div>
+                <img :src="imgProfesseur" alt="">
+                <div>
+                    <h1>Bienvenue sur Pokemonator !</h1>
+                    <p>Pensez à un pokemon, et quand vous êtes prêt, appuyez sur 'commencer' !</p>
+                    <button @click="start">Commencer !</button>
+                </div>
+            </div>
+        </div>
+        <div :class="{hidden: !isAsking}">
             <Personnage :question="question"
                 @oui="updateData('oui', question, workingOnPokemons, impossiblePokemons), updateState()"
                 @non="updateData('non', question, workingOnPokemons, impossiblePokemons), updateState()">
@@ -63,9 +86,6 @@
         <div :class="{hidden: !isGuessing}">
             <Guesser :pokemons="workingOnPokemons" :replay="replay"/>
         </div>
-        <div :class="{hidden: !isLoosing}">
-
-        </div>
     </main>
 </template>
 
@@ -78,8 +98,28 @@
         margin: auto;
     }
 
+    main > div:first-child > div {
+        display: flex;
+        align-items: center;
+    }
+    
+    main > div:first-child > div > div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1 {
+        text-align: center
+    }
+
     .hidden {
         display: none;
+    }
+
+    img {
+        height: 80vh;
+        margin-right: 30px;
     }
 
     /* On screens that are 600px or less, set the background color to olive */
